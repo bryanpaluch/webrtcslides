0a0ea6d4572a102d3462b5c65b09c102534b5c8d
Bryan Paluch <bryanpaluch@gmail.com>
2012-12-13 13:54:43 -0500

    commiting webrtc slides

---
+++ plugin/webrtc-server/index.js
@@ -0,0 +1,82 @@
+var express   = require('express');
+var fs        = require('fs');
+var io        = require('socket.io');
+var _         = require('underscore');
+var Mustache  = require('mustache');
+
+var app       = express.createServer();
+var staticDir = express.static;
+
+io            = io.listen(app);
+
+var opts = {
+	port :      1947,
+	baseDir :   __dirname + '/../../'
+};
+
+io.sockets.on('connection', function(socket) {
+	socket.on('slidechanged', function(slideData) {
+		socket.broadcast.emit('slidedata', slideData);
+	});
+	socket.on('fragmentchanged', function(fragmentData) {
+		socket.broadcast.emit('fragmentdata', fragmentData);
+	});
+  
+  socket.on('rtc_init_receiver', function(data){
+    console.log('received joined');
+    this.join('receiver');
+  });
+  
+  socket.on('rtc_init_offerer', function(data){
+    console.log('offerer has joined');
+    this.join('offerer');
+  });
+  
+  socket.on('rtc_answer', function(data){
+    console.log(data);
+    io.sockets.in('offerer').emit('rtc_answer', data);
+  });
+
+  socket.on('rtc_request', function(data){
+    console.log(data);
+    io.sockets.in('receiver').emit('rtc_request', data);
+  });
+});
+
+app.configure(function() {
+	[ 'css', 'js', 'images', 'plugin', 'lib' ].forEach(function(dir) {
+		app.use('/' + dir, staticDir(opts.baseDir + dir));
+	});
+});
+
+app.get("/receiver", function(req, res){
+  fs.createReadStream(opts.baseDir + '/receiver.html').pipe(res);
+});
+
+app.get("/", function(req, res) {
+	fs.createReadStream(opts.baseDir + '/index.html').pipe(res);
+});
+
+app.get("/notes/:socketId", function(req, res) {
+
+	fs.readFile(opts.baseDir + 'plugin/notes-server/notes.html', function(err, data) {
+		res.send(Mustache.to_html(data.toString(), {
+			socketId : req.params.socketId
+		}));
+	});
+	// fs.createReadStream(opts.baseDir + 'notes-server/notes.html').pipe(res);
+});
+
+// Actually listen
+app.listen(opts.port || null);
+
+var brown = '\033[33m',
+	green = '\033[32m',
+	reset = '\033[0m';
+
+var slidesLocation = "http://localhost" + ( opts.port ? ( ':' + opts.port ) : '' );
+
+console.log( brown + "reveal.js - Speaker Notes" + reset );
+console.log( "1. Open the slides at " + green + slidesLocation + reset );
+console.log( "2. Click on the link your JS console to go to the notes page" );
+console.log( "3. Advance through your slides and your notes will advance automatically" );
